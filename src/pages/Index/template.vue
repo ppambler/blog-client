33729a77428436f8114332fbc0812d37f4464088
Ambler <allinlbj@gmail.com>
2018-09-29 19:53:40 +0800

    install less & test less

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="index">
-    <p>首页</p> 
+    <p>首页</p>
+    <p class="theme">我是主题色</p> 
   </div>
 </template>
 
 <script src='./template.js'></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src='./template.css'></style>
+<style scoped lang='less' src='./template.less'></style>
