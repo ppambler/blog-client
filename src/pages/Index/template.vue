a3fb162a5cefa5370aec69a9033a689e67e3b185
Ambler <allinlbj@gmail.com>
2018-09-29 18:38:03 +0800

    路由初始化

---
+++ src/pages/Index/template.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="index">
+    <p>首页</p> 
+  </div>
+</template>
+
+<script src='./template.js'></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src='./template.css'></style>
