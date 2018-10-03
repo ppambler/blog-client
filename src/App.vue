4ae737c9eac320661395baf7ff43ce2e7cbdff62
Ambler <allinlbj@gmail.com>
2018-10-03 16:26:42 +0800

    为App.vue添加布局样式

--- src/App.vue
+++ src/App.vue
@@ -21,13 +21,45 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less'>
+
+@import './assets/common.less';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  grid-template-columns: 12% auto 12%;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas: "header header header"
+                       ".      main  ."
+                       "footer footer footer";
+
+  #header {
+    grid-area: header;
+    padding-left: 12%;
+    padding-right: 12%;
+  }
+
+  #main {
+    grid-area: main;
+  }
+
+  #footer {
+    grid-area: footer;
+    padding-left: 12%;
+    padding-right: 12%;
+  }
+
+}
+
+@media (max-width: 768px) {
+  #app {
+    grid-template-columns: 10px auto 10px;
+
+    #header, #footer {
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+  }
+
 }
 </style>
