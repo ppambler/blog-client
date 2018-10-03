c5c195ff4c169766467b0354bec4054e996dcfef
Ambler <allinlbj@gmail.com>
2018-10-03 16:54:06 +0800

    为header和footer组件添加特别的样式

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,3 +3,15 @@
         <p>@ xiedaimala.com 2018 wechat: xiedaimala01</p>
     </footer>
 </template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
+
