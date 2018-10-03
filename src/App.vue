e8cb045ac120b655e27d0bfc6116b88b302c32f0
Ambler <allinlbj@gmail.com>
2018-10-03 16:01:30 +0800

    为App.vue添加两个组件footer和header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header id="header"></Header>
+    <main id="main">
+      <router-view/>
+    </main>
+    <Footer id="footer"></Footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
---
+++ src/components/footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer>
+        <p>@ xiedaimala.com 2018 wechat: xiedaimala01</p>
+    </footer>
+</template>
