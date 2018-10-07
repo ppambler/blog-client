4aecc0ca4d003906b1df72959fab74e4a57d093d
Ambler <allinlbj@gmail.com>
2018-10-07 20:22:47 +0800

    为路由添加参数以及权限验证

--- src/router/index.js
+++ src/router/index.js
@@ -9,9 +9,13 @@ import Create from '@/pages/Create/template.vue'
 import Register from '@/pages/Register/template.vue'
 import My from '@/pages/My/template.vue'
 
+import store from '../store'
+
+window.store = store
+
 Vue.use(Router)
  
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -22,28 +26,52 @@ export default new Router({
       component: Login
     },
     {
-      path: '/detail',
-      component: Detail
-    },
-    {
-      path: '/edit',
-      component: Edit
-    },
-    {
-      path: '/user',
-      component: User
-    },
-    {
-      path: '/create',
-      component: Create
-    },
-    {
       path: '/register',
       component: Register
     },
     {
+      path: '/detail/:blogId',
+      component: Detail
+    },
+    {
+      path: '/edit/:blogId',
+      component: Edit,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/user/:userId',
+      component: User
+    },
+    {
+      path: '/create',
+      component: Create,
+      meta: { requiresAuth: true }
+    },
+    {
       path: '/my',
-      component: My
+      component: My,
+      meta: { requiresAuth: true }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    store.dispatch('checkLogin').then( isLogin => {
+      if (!store.getters.isLogin) {
+        next({
+          path: '/login',
+          query: { redirect: to.fullPath }
+        })
+      } else {
+        next()
+      }
+    })
+  } else {
+    next() // 确保一定要调用 next()
+  }
+})
+
+export default router
