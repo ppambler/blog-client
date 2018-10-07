1d4b34ae848c56b8d03e9fe1208437f11062a1ee
Ambler <allinlbj@gmail.com>
2018-10-07 15:47:15 +0800

    完善登录页面，使用vuex

--- src/pages/Login/template.js
+++ src/pages/Login/template.js
@@ -1,7 +1,22 @@
+import { mapActions } from 'vuex'
+
 export default {
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        username: '',
+        password: ''
+      }
+    },
+
+    methods: {
+      ...mapActions(['login']),
+
+      // 根据双向绑定可以拿到用户输入的数据，经过后端提供的接口验证后，假如成功就跳转到首页！
+      onLogin() {
+        this.login({ username: this.username, password: this.password })
+          .then(() => {
+            this.$router.push({path: '/'})
+          })
       }
     }
 }
