aa1937a3e05a23de0b148d971ed739f3cb9f8039
Ambler <allinlbj@gmail.com>
2018-10-07 20:46:39 +0800

    打包发布，加载层面的解耦

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+/*
 import Index from '@/pages/Index/template.vue'
 import Login from '@/pages/Login/template.vue'
 import Detail from '@/pages/Detail/template.vue'
@@ -8,13 +9,14 @@ import User from '@/pages/User/template.vue'
 import Create from '@/pages/Create/template.vue'
 import Register from '@/pages/Register/template.vue'
 import My from '@/pages/My/template.vue'
+*/
 
 import store from '../store'
 
-window.store = store
+// window.store = store
 
 Vue.use(Router)
- 
+/* 
 const router = new Router({
   routes: [
     {
@@ -50,6 +52,47 @@ const router = new Router({
     {
       path: '/my',
       component: My,
+      meta: { requiresAuth: true }
+    }
+  ]
+})
+*/
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/pages/Index/template.vue')
+    },
+    {
+      path: '/login',
+      component: () => import('@/pages/Login/template.vue')
+    },
+    {
+      path: '/register',
+      component: () => import('@/pages/Register/template.vue')
+    },
+    {
+      path: '/detail/:blogId',
+      component: () => import('@/pages/Detail/template.vue')
+    },
+    {
+      path: '/edit/:blogId',
+      component: () => import('@/pages/Edit/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/user/:userId',
+      component: () => import('@/pages/User/template.vue')
+    },
+    {
+      path: '/create',
+      component: () => import('@/pages/Create/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/my',
+      component: () => import('@/pages/My/template.vue'),
       meta: { requiresAuth: true }
     }
   ]
