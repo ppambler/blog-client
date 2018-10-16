8982ceebf3726663cbd81853406c0af831b64218
Ambler <allinlbj@gmail.com>
2018-10-16 17:23:46 +0800

    没有优化的字数限制校验

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -4,13 +4,38 @@ export default {
         title: '',
         description: '',
         content: '',
-        atIndex: false
+        atIndex: false,
       }
     },
 
     methods: {
       onCreate() {
-        
+
+      },
+      onInputTitle() {
+        let titleNum = parseInt(this.title.length)
+        if(titleNum === 30 ) {
+          this.$refs.titleStr.$el.children[0].classList.add('strStyle')
+        } else {
+          this.$refs.titleStr.$el.children[0].classList.remove('strStyle')
+        }
+      },
+      onInputDescription() {
+        let descriptionNum = parseInt(this.description.length)
+        if( descriptionNum === 300 ) {
+          this.$refs.descriptionStr.$el.children[0].classList.add('strStyle')
+        } else {
+          this.$refs.descriptionStr.$el.children[0].classList.remove('strStyle')
+        }
+      },
+      onInputContent() {
+        let contentNum = parseInt(this.content.length)
+        if(contentNum === 6000 ) {
+          this.$refs.contentStr.$el.children[0].classList.add('strStyle')
+        } else {
+          this.$refs.contentStr.$el.children[0].classList.remove('strStyle')
+        }
       }
+
     }
 }
--- src/pages/Create/template.vue
+++ src/pages/Create/template.vue
@@ -2,14 +2,14 @@
   <div id="edit">
     <h1>创建文章</h1>
     <h3>文章标题</h3>
-    <el-input v-model="title"></el-input>
-    <p class="msg">限12个字</p>
+    <el-input ref="titleStr" v-model="title" @input="onInputTitle" v-bind:style="maxStyle" maxlength=30></el-input>
+    <p class="msg">还可以输入{{30-title.length}}个字，限30个字</p>
     <h3>内容简介</h3>
-    <el-input type="textarea" v-model="description" :autosize="{ minRows: 2, maxRows: 6}"></el-input>
-    <p class="msg">限100个字</p>
+    <el-input ref="descriptionStr" type="textarea" v-model="description" @input="onInputDescription" :autosize="{ minRows: 2, maxRows: 6}" maxlength=300></el-input>
+    <p class="msg">还可以输入{{300-description.length}}个字，限300个字</p>
     <h3>文章内容</h3>
-    <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}"></el-input>
-    <p class="msg">限5000个字</p>
+    <el-input ref="contentStr" type="textarea" v-model="content" @input="onInputContent" :autosize="{ minRows: 4, maxRows: 30}" maxlength=6000></el-input>
+    <p class="msg">还可以输入{{6000-content.length}}个字，限6000个字</p>
     <p>
       <label>是否展示到首页</label>
       <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
